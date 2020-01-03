c5d3a86f3d64fd733441bb8805546a23b5826430
Adam Lovatt <jalovatt@hotmail.com>
2020-01-03 11:57:33 -0700

    Adds room generation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 use std::cmp;
 use tcod::colors::*;
 use tcod::console::*;
+use rand::Rng;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
 
 const MAP_WIDTH: i32 = 80;
 const MAP_HEIGHT: i32 = 45;
+
+const ROOM_MAX_SIZE: i32 = 10;
+const ROOM_MIN_SIZE: i32 = 6;
+
+const MAX_ROOMS: i32 = 30;
 
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
 const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150, };
@@ -72,6 +78,20 @@ impl Rect {
   pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
     Rect { x1: x, y1: y, x2: x + w, y2: y + h }
   }
+
+  pub fn center(&self) -> (i32, i32) {
+    let center_x = (self.x1 + self.x2) / 2;
+    let center_y = (self.y1 + self.y2) / 2;
+
+    (center_x, center_y)
+  }
+
+  pub fn intersects_with(&self, other: &Rect) -> bool {
+    (self.x1 <= other.x2)
+      && (self.x2 >= other.x1)
+      && (self.y1 <= other.y2)
+      && (self.y2 >= other.y1)
+  }
 }
 
 type Map = Vec<Vec<Tile>>;
@@ -94,15 +114,50 @@ fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
   }
 }
 
-fn make_map() -> Map {
+fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
+  for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
+    map[x as usize][y as usize] = Tile::empty();
+  }
+}
+
+fn make_map(player: &mut Object) -> Map {
   let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
 
-  let room1 = Rect::new(20, 15, 10, 15);
-  let room2 = Rect::new(50, 15, 10, 15);
-  create_room(room1, &mut map);
-  create_room(room2, &mut map);
+  let mut rooms = vec![];
 
-  create_h_tunnel(25, 55, 23, &mut map);
+  for _ in 0..MAX_ROOMS {
+    let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+    let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
+    let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
+    let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
+
+    let new_room = Rect::new(x, y, w, h);
+
+    let failed = rooms.iter().any(|other| new_room.intersects_with(other));
+
+    if failed { continue; }
+
+    create_room(new_room, &mut map);
+
+    let (new_x, new_y) = new_room.center();
+
+    if rooms.is_empty() {
+      player.x = new_x;
+      player.y = new_y;
+    } else {
+      let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
+
+      if rand::random() {
+        create_h_tunnel(prev_x, new_x, prev_y, &mut map);
+        create_v_tunnel(prev_y, new_y, new_x, &mut map);
+      } else {
+        create_v_tunnel(prev_y, new_y, prev_x, &mut map);
+        create_h_tunnel(prev_x, new_x, new_y, &mut map);
+      }
+    }
+
+    rooms.push(new_room);
+  }
 
   map
 }
@@ -170,14 +225,14 @@ fn main() {
 
   tcod::system::set_fps(LIMIT_FPS);
 
-  let game = Game {
-    map: make_map(),
-  };
-
-  let player = Object::new(25, 23, '@', WHITE);
+  let player = Object::new(0, 0, '@', WHITE);
   let npc = Object::new(SCREEN_WIDTH / 2 - 5, SCREEN_HEIGHT / 2, '@', YELLOW);
 
   let mut objects = [player, npc];
+
+  let game = Game {
+    map: make_map(&mut objects[0]),
+  };
 
   while !tcod.root.window_closed() {
     tcod.con.clear();
