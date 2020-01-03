47f5d8e635c42a45c14a7e59c3d35ede86bf3923
Adam Lovatt <jalovatt@hotmail.com>
2020-01-03 12:15:22 -0700

    Adds FOV

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::cmp;
 use tcod::colors::*;
 use tcod::console::*;
+use tcod::map::{FovAlgorithm, Map as FovMap};
 use rand::Rng;
 
 const SCREEN_WIDTH: i32 = 80;
@@ -14,14 +15,21 @@ const ROOM_MIN_SIZE: i32 = 6;
 
 const MAX_ROOMS: i32 = 30;
 
+const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
+const FOV_LIGHT_WALLS: bool = true;
+const TORCH_RADIUS: i32 = 10;
+
 const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+const COLOR_LIGHT_WALL: Color = Color { r: 130, g: 110, b: 50 };
 const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150, };
+const COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b: 50, };
 
 const LIMIT_FPS: i32 = 20;
 
 struct Tcod {
   root: Root,
   con: Offscreen,
+  fov: FovMap,
 }
 
 #[derive(Debug)]
@@ -162,20 +170,31 @@ fn make_map(player: &mut Object) -> Map {
   map
 }
 
-fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object]) {
+fn render_all(tcod: &mut Tcod, game: &Game, objects: &[Object], fov_recompute: bool) {
+  if fov_recompute {
+    let player = &objects[0];
+    tcod.fov.compute_fov(player.x, player.y, TORCH_RADIUS, FOV_LIGHT_WALLS, FOV_ALGO);
+  }
+
   for object in objects {
-    object.draw(&mut tcod.con);
+    if tcod.fov.is_in_fov(object.x, object.y) {
+      object.draw(&mut tcod.con);
+    }
   }
 
   for y in 0..MAP_HEIGHT {
     for x in 0..MAP_WIDTH {
+      let visible = tcod.fov.is_in_fov(x, y);
       let wall = game.map[x as usize][y as usize].block_sight;
 
-      if wall {
-        tcod.con.set_char_background(x, y, COLOR_DARK_WALL, BackgroundFlag::Set);
-      } else {
-        tcod.con.set_char_background(x, y, COLOR_DARK_GROUND, BackgroundFlag::Set);
-      }
+      let color = match (visible, wall) {
+        (false, true) => COLOR_DARK_WALL,
+        (false, false) => COLOR_DARK_GROUND,
+        (true, true) => COLOR_LIGHT_WALL,
+        (true, false) => COLOR_LIGHT_GROUND,
+      };
+
+      tcod.con.set_char_background(x, y, color, BackgroundFlag::Set);
     }
   }
 
@@ -219,9 +238,11 @@ fn main() {
     .title("Rust/libtcod tutorial")
     .init();
 
-  let con = Offscreen::new(MAP_WIDTH, MAP_HEIGHT);
-
-  let mut tcod = Tcod { root, con };
+  let mut tcod = Tcod {
+    root,
+    con: Offscreen::new(MAP_WIDTH, MAP_HEIGHT),
+    fov: FovMap::new(MAP_WIDTH, MAP_HEIGHT),
+  };
 
   tcod::system::set_fps(LIMIT_FPS);
 
@@ -234,14 +255,30 @@ fn main() {
     map: make_map(&mut objects[0]),
   };
 
+  for y in 0..MAP_HEIGHT {
+    for x in 0..MAP_WIDTH {
+      tcod.fov.set(
+        x,
+        y,
+        !game.map[x as usize][y as usize].block_sight,
+        !game.map[x as usize][y as usize].blocked,
+      );
+    }
+  }
+
+  let mut previous_player_position = (-1, -1);
+
   while !tcod.root.window_closed() {
     tcod.con.clear();
 
-    render_all(&mut tcod, &game, &objects);
+    let fov_recompute = previous_player_position != (objects[0].x, objects[0].y);
+    render_all(&mut tcod, &game, &objects, fov_recompute);
 
     tcod.root.flush();
 
     let player = &mut objects[0];
+
+    previous_player_position = (player.x, player.y);
 
     let exit = handle_keys(&mut tcod, &game, player);
     if exit { break; }
