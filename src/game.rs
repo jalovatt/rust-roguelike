0da6c48a07e20e3def3ed4ef445ca56b7dd6cd8a
Adam Lovatt <jalovatt@hotmail.com>
2020-01-03 13:22:23 -0700

    Refactors to multiple files

---
+++ src/game.rs
@@ -0,0 +1,5 @@
+use crate::map::Map;
+
+pub struct Game {
+  pub map: Map,
+}
--- src/main.rs
+++ src/main.rs
@@ -1,174 +1,42 @@
-use std::cmp;
 use tcod::colors::*;
 use tcod::console::*;
 use tcod::map::{FovAlgorithm, Map as FovMap};
-use rand::Rng;
 
-const SCREEN_WIDTH: i32 = 80;
-const SCREEN_HEIGHT: i32 = 50;
+mod tile;
+mod map;
+mod object;
+mod game;
 
-const MAP_WIDTH: i32 = 80;
-const MAP_HEIGHT: i32 = 45;
+use map as Map;
+use object::Object;
+use game::Game;
 
-const ROOM_MAX_SIZE: i32 = 10;
-const ROOM_MIN_SIZE: i32 = 6;
+static SCREEN_WIDTH: i32 = 80;
+static SCREEN_HEIGHT: i32 = 50;
 
-const MAX_ROOMS: i32 = 30;
+static MAP_WIDTH: i32 = 80;
+static MAP_HEIGHT: i32 = 45;
 
-const FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
-const FOV_LIGHT_WALLS: bool = true;
-const TORCH_RADIUS: i32 = 10;
+static ROOM_MAX_SIZE: i32 = 10;
+static ROOM_MIN_SIZE: i32 = 6;
 
-const COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
-const COLOR_LIGHT_WALL: Color = Color { r: 130, g: 110, b: 50 };
-const COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150, };
-const COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b: 50, };
+static MAX_ROOMS: i32 = 30;
 
-const LIMIT_FPS: i32 = 20;
+static FOV_ALGO: FovAlgorithm = FovAlgorithm::Basic;
+static FOV_LIGHT_WALLS: bool = true;
+static TORCH_RADIUS: i32 = 10;
+
+static COLOR_DARK_WALL: Color = Color { r: 0, g: 0, b: 100 };
+static COLOR_LIGHT_WALL: Color = Color { r: 130, g: 110, b: 50 };
+static COLOR_DARK_GROUND: Color = Color { r: 50, g: 50, b: 150, };
+static COLOR_LIGHT_GROUND: Color = Color { r: 200, g: 180, b: 50, };
+
+static LIMIT_FPS: i32 = 20;
 
 struct Tcod {
   root: Root,
   con: Offscreen,
   fov: FovMap,
-}
-
-#[derive(Debug)]
-struct Object {
-  x: i32,
-  y: i32,
-  char: char,
-  color: Color,
-}
-
-impl Object {
-  pub fn new(x: i32, y: i32, char: char, color: Color) -> Self {
-    Object { x, y, char, color }
-  }
-
-  pub fn move_by(&mut self, dx: i32, dy: i32, game: &Game) {
-    if !game.map[(self.x + dx) as usize][(self.y + dy) as usize].blocked {
-      self.x += dx;
-      self.y += dy;
-    }
-  }
-
-  pub fn draw(&self, con: &mut dyn Console) {
-    con.set_default_foreground(self.color);
-    con.put_char(self.x, self.y, self.char, BackgroundFlag::None);
-  }
-}
-
-#[derive(Clone, Copy, Debug)]
-struct Tile {
-  blocked: bool,
-  block_sight: bool,
-  explored: bool,
-}
-
-impl Tile {
-  pub fn empty() -> Self {
-    Tile { blocked: false, explored: false, block_sight: false }
-  }
-
-  pub fn wall() -> Self {
-    Tile { blocked: true, explored: false, block_sight: true }
-  }
-}
-
-#[derive(Clone, Copy, Debug)]
-struct Rect {
-  x1: i32,
-  y1: i32,
-  x2: i32,
-  y2: i32,
-}
-
-impl Rect {
-  pub fn new(x: i32, y: i32, w: i32, h: i32) -> Self {
-    Rect { x1: x, y1: y, x2: x + w, y2: y + h }
-  }
-
-  pub fn center(&self) -> (i32, i32) {
-    let center_x = (self.x1 + self.x2) / 2;
-    let center_y = (self.y1 + self.y2) / 2;
-
-    (center_x, center_y)
-  }
-
-  pub fn intersects_with(&self, other: &Rect) -> bool {
-    (self.x1 <= other.x2)
-      && (self.x2 >= other.x1)
-      && (self.y1 <= other.y2)
-      && (self.y2 >= other.y1)
-  }
-}
-
-type Map = Vec<Vec<Tile>>;
-
-struct Game {
-  map: Map,
-}
-
-fn create_room(room: Rect, map: &mut Map) {
-  for x in (room.x1 + 1)..room.x2 {
-    for y in (room.y1 + 1)..room.y2 {
-      map[x as usize][y as usize] = Tile::empty();
-    }
-  }
-}
-
-fn create_h_tunnel(x1: i32, x2: i32, y: i32, map: &mut Map) {
-  for x in cmp::min(x1, x2)..(cmp::max(x1, x2) + 1) {
-    map[x as usize][y as usize] = Tile::empty();
-  }
-}
-
-fn create_v_tunnel(y1: i32, y2: i32, x: i32, map: &mut Map) {
-  for y in cmp::min(y1, y2)..(cmp::max(y1, y2) + 1) {
-    map[x as usize][y as usize] = Tile::empty();
-  }
-}
-
-fn make_map(player: &mut Object) -> Map {
-  let mut map = vec![vec![Tile::wall(); MAP_HEIGHT as usize]; MAP_WIDTH as usize];
-
-  let mut rooms = vec![];
-
-  for _ in 0..MAX_ROOMS {
-    let w = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
-    let h = rand::thread_rng().gen_range(ROOM_MIN_SIZE, ROOM_MAX_SIZE + 1);
-    let x = rand::thread_rng().gen_range(0, MAP_WIDTH - w);
-    let y = rand::thread_rng().gen_range(0, MAP_HEIGHT - h);
-
-    let new_room = Rect::new(x, y, w, h);
-
-    let failed = rooms.iter().any(|other| new_room.intersects_with(other));
-
-    if failed { continue; }
-
-    create_room(new_room, &mut map);
-
-    let (new_x, new_y) = new_room.center();
-
-    if rooms.is_empty() {
-      player.x = new_x;
-      player.y = new_y;
-    } else {
-      let (prev_x, prev_y) = rooms[rooms.len() - 1].center();
-
-      if rand::random() {
-        create_h_tunnel(prev_x, new_x, prev_y, &mut map);
-        create_v_tunnel(prev_y, new_y, new_x, &mut map);
-      } else {
-        create_v_tunnel(prev_y, new_y, prev_x, &mut map);
-        create_h_tunnel(prev_x, new_x, new_y, &mut map);
-      }
-    }
-
-    rooms.push(new_room);
-  }
-
-  map
 }
 
 fn render_all(tcod: &mut Tcod, game: &mut Game, objects: &[Object], fov_recompute: bool) {
@@ -259,7 +127,7 @@ fn main() {
   let mut objects = [player, npc];
 
   let mut game = Game {
-    map: make_map(&mut objects[0]),
+    map: Map::make_map(&mut objects[0]),
   };
 
   for y in 0..MAP_HEIGHT {
